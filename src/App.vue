7020c6528012d338b5b62fcec43ded8ae4d5a9f8
Mirko Cifelli <146765212+MirkoCifelli@users.noreply.github.com>
2024-01-17 14:21:34 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<script>
+// importo il componente
+import AppHeader from './components/AppHeader.vue';
+//  2 dichiaro il componente all interno dell oggetto
+//  3 utilizzo del componente
+export default {
+    data() {
+        return { 
+            count: 0 
+        }
+    },
+    // 2
+    components:{
+        AppHeader
+    }
+   
+}
+</script>
+
+<template>
+
+    <div>
+        <!-- 3 -->
+        <AppHeader/>
+        <h1>
+            Ciao
+        </h1>
+        <p>
+            {{ count }}
+        </p>
+        <button @click="count++">
+            Incrementa il count
+        </button>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+    data() {
+        return { 
+            title: 'ciao ragazzi'
+        }
+    },
+   
+}
+</script>
+
+<template>
+    <header>
+        <h1>
+            {{ title }}
+        </h1>
+    </header>
+</template>
+
+<style scoped>
+header{
+    padding: 20px;
+    background-color: aquamarine;
+}
+h1{
+    color: lightcoral;
+    text-align: center;
+}
+</style>
